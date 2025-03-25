b04fcd61c7d7b3c8257bce1402debbb0a369b804
coder-j25 <justinee.hanson@outlook.com>
2025-03-25 15:36:59 -0700

    experience component

--- client/src/views/pages/AboutMe.jsx
+++ client/src/views/pages/AboutMe.jsx
@@ -1,18 +1,47 @@
 
 import Introduction from "../components/Introduction"
+import Timeline from "../components/Timeline";
 import React from "react"
 
 
 
 const AboutMe = () => {
-
+    const timelineData = [
+        {
+          date: "March 2025 - Current",
+          title: "Banyan Labs | Web Developer",
+          description:
+            "Developed and maintained responsive web applications using HTML, CSS, JavaScript, and React, ensuring cross-browser compatibility and mobile responsiveness. Collaborated with team members to design, build, and test features using React.js and Node.js, improving application functionality.",
+        },
+  
+      ];
+      const timelineSchoolData = [
+        {
+          date: "February 2025- Current",
+          title: "Coursera | UX/UI Design Certificate",
+         
+        },
+        {
+            date: "July 2023 - June 2024",
+            title: "Persevere | Full Stack Developer Bootcamp",
+           
+          },
+          {
+            date: "February 2010",
+            title: "GED | Pima Community College ",
+           
+          },
+  
+  
+      ];
+    
 
     return (
     <>
    
 <div className="mt-20 ml-16 mr-16 p-2">
-<div className="leftside">
- <img className="profilePic w1/2" src={new URL('../assets/profile-pics/profileWborder.jpg', import.meta.url).href} alt="profile-pic" />
+<div className=" left-0 w-64 h-full bg-white-800 text-black">
+ <img className="profilePic w1/2" src={new URL('../assets/profilePics/aboutPic.png', import.meta.url).href} alt="profile-pic" />
 <div className="pt-6">
    
    <p className="text-[1em] pl-4 text-gray-500">Summary of</p>
@@ -26,6 +55,13 @@ const AboutMe = () => {
       description="Full Stack Developer skilled in end-to-end application development, agile teamwork, and cutting-edge technologies. 
       Experienced in building feature-rich solutions with seamless functionality and engaging UI/UX design."
     />
+    <Timeline 
+    category="Experience"
+    events={timelineData} />
+    <Timeline 
+    category="Education/Certification"e
+    events={timelineSchoolData} />
+    
 </div>
     </>
 )}
