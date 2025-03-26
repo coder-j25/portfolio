93d46ececabcf878260b03d90496a076747d3fe2
coder-j25 <justinee.hanson@outlook.com>
2025-03-26 07:59:36 -0700

    work section header

--- client/src/views/pages/Home.jsx
+++ client/src/views/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <>
          
 
-        <div className="pt-12"> 
+         <div className="pt-12"> 
 <div className="flex flex-col items-center bg-white md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-16 m-auto">
     <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={new URL('../assets/profile-pics/profile-pic.jpg', import.meta.url).href} alt="profile-pic" />
     <div className="flex flex-col justify-between p-4 leading-normal">
@@ -21,7 +21,7 @@ const Home = () => {
 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white p-8">Projects</h5>
 
 </div>
-</div>   
+</div>
         </>
     )
 }
