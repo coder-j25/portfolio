08778965f273810400cf3f14396855884415958b
coder-j25 <justinee.hanson@outlook.com>
2025-03-27 10:27:16 -0700

    minor style adjustments

--- client/src/views/pages/Home.jsx
+++ client/src/views/pages/Home.jsx
@@ -1,4 +1,4 @@
-import workpic from '../../assets/portfolio/workPic.jpg'
+
 import React from "react"
 import { Link } from "react-router-dom";
 
@@ -7,7 +7,9 @@ const Home = () => {
         <>
           <div className="pt-12"> 
 <div className="flex flex-col items-center bg-white md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-16 m-auto">
-    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={workpic} alt="profile-pic" />
+    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+    src={new URL('../../assets/portfolio/workPic.jpg', import.meta.url).href}
+    alt="profile-pic" />
     <div className="flex flex-col justify-between p-4 leading-normal">
         <h1  className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Justine&#233; Hanson</h1>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Software Developer</h5>
