3324b5d9c3939aabdea0346728981a412884d3d3
coder-j25 <justinee.hanson@outlook.com>
2025-04-01 09:25:09 -0700

    contact form

--- client/src/views/pages/Contact.jsx
+++ client/src/views/pages/Contact.jsx
@@ -1,23 +1,27 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
 
 
 const Contact = () => {
     return (
         <>
-            <div className="m-36">
-                <div className="flex flex-col">
-                    <h1 className="text-2xl font-extrabold text-black dark:text-black pb-8">Contact Me</h1>
-                    <div className="pl-10 ">
-                       <a href="https://github.com/coder-j25"><img className="w-[8%] pb-8"
-                            src={new URL('../../assets/icons/github-black.png', import.meta.url).href} /></a>
-                        <a href="https://www.linkedin.com/in/justinee-hanson-5a938134a/"><img className="w-[8%]"
-                            src={new URL('../../assets/icons/linkedin_icon_black.png', import.meta.url).href} /></a>
-                    </div>
-                </div>
-                <div>
 
-                </div>
-            </div>
+
+<div className="mt-26 flex flex-col justify-center">
+              <div>
+               <h1 className="text-2xl font-extrabold text-black dark:text-black pb-8 text-center ">Contact Me</h1>
+               <div className="flex justify-center gap-4 pb-8">
+<a href="https://github.com/coder-j25">
+<img className="w-12" src={new URL('../../assets/icons/github-black.png', import.meta.url).href} alt="GitHub" />
+</a>
+<a href="https://www.linkedin.com/in/justinee-hanson-5a938134a/">
+<img className="w-12" src={new URL('../../assets/icons/linkedin_icon_black.png', import.meta.url).href} alt="LinkedIn" />
+</a>
+</div></div> 
+
+<ContactForm/></div>
+
+
 
         </>
     )
